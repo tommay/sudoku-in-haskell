bdd9862ed9909fab02314b619cf7ca7d2cf6c4bf
Tom May <tom@tommay.net>
2016-09-12 14:27:57 -0700

    Use Sequence instead of Vector.

--- Cells.hs
+++ Cells.hs
@@ -11,30 +11,29 @@ import qualified Cell
 import Cell (Cell)
 import qualified Possible
 import Possible (Possible)
-import qualified Data.Vector as Vector
-import Data.Vector (Vector)
+import qualified Data.Sequence as Sequence
+import Data.Sequence (Seq)
+import qualified Data.Foldable as Foldable
 
 -- Cells holds the state of each Cell, as a tuple containing one Cell
 -- for each position on the board, numbered 0 through 80.  This allow
 -- us to (somewhat) abstract out the data type Puzzle uses to store
 -- Cells.
 
-data Cells = Cells (Vector Cell) deriving (Show)
+data Cells = Cells (Seq Cell) deriving (Show)
 
 -- Returns a new Cells with fresh Cells.
 
 new :: Cells
-new = Cells $ Vector.generate 81 Cell.new
+new = Cells $ Sequence.fromFunction 81 Cell.new
 
 -- Returns a new Cells with func applied to the indexth element.
 
 update :: Cells -> Int -> (Cell -> Cell) -> Cells
 update this index func =
-  let Cells vector = this
-      elem = vector Vector.! index
-      newElem = func elem
-      newVector = vector Vector.// [(index, newElem)]
-  in Cells newVector
+  let Cells seq = this
+      newSeq = Sequence.adjust func index seq
+  in Cells newSeq
 
 -- Returns the unplaced Cell with the smallest set of possibilities.
 -- This is used to find the best Cell to make a guess for to minimize
@@ -49,8 +48,8 @@ minByPossibleSize this =
         case Cell.getPossible cell of
           Just possible -> Possible.size possible
           _ -> 10
-      Cells vector = this
-  in Vector.minimumBy (\a b -> compare (toSize a) (toSize b)) vector
+      Cells seq = this
+  in Foldable.minimumBy (\a b -> compare (toSize a) (toSize b)) seq
 
 -- Update the numbered Cells in ExclusionList to remove Digit from
 -- their Possible lists.
@@ -68,5 +67,5 @@ doExclusions this digit exclusionList =
 
 toString :: Cells -> String
 toString this =
-  let Cells vector = this
-  in map Cell.toChar $ Vector.toList vector
+  let Cells seq = this
+  in map Cell.toChar $ Foldable.toList seq
