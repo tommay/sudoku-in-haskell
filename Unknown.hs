fa602ec3b9749468ef70a467fbe46356273ec23a
Tom May <tom@tommay.net>
2016-09-16 12:42:02 -0700

    Add missing False case.  Maybe if/then/else is better after all.

--- Unknown.hs
+++ Unknown.hs
@@ -36,6 +36,7 @@ place :: Unknown -> Unknown -> Int -> Unknown
 place this other digit =
   case isExcludedBy this other of
     True -> this { possible = List.delete digit $ possible this }
+    False -> this
 
 -- Returns true if this and Other are in the same row, column, or
 -- square, else false.
