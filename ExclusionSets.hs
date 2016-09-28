10405f22149d7783ff32ebd6fc06ec15c921449d
Tom May <tom@tommay.net>
2016-09-28 11:10:27 -0700

    Factor out ExclusionSets.squares.

--- ExclusionSets.hs
+++ ExclusionSets.hs
@@ -1,5 +1,6 @@
 module ExclusionSets (
   ExclusionSets.exclusionSets,
+  ExclusionSets.squares,
   ExclusionSets.exclusionSetsTuple,  -- XXX until TrickySets is split out of Puzzle.
 ) where
 
@@ -11,6 +12,18 @@ exclusionSets =
   let (rows, cols, squares) = exclusionSetsTuple
   in rows ++ cols ++ squares
 
+-- Create an ExclusionSet for each square.
+--
+squares :: [[Int]]
+squares = map (\ square ->
+    let
+      -- row and col of upper left corner of square
+      row = square `div` 3 * 3
+      col = square `mod` 3 * 3
+    in
+      [(row + n `div` 3)*9 + (col + n `mod` 3) | n <- [0..8]])
+  [0..8]
+
 exclusionSetsTuple :: ([[Int]], [[Int]], [[Int]])
 exclusionSetsTuple =
  let
@@ -20,15 +33,5 @@ exclusionSetsTuple =
 
    -- Create an ExclusionSet for each column.
    cols = [[row*9 + col | row <- [0..8]] | col <- [0..8]]
-
-   -- Create an ExclusionSet for each square.
-   squares = map (\ square ->
-       let
-         -- row and col of upper left corner of square
-         row = square `div` 3 * 3
-         col = square `mod` 3 * 3
-       in
-         [(row + n `div` 3)*9 + (col + n `mod` 3) | n <- [0..8]])
-     [0..8]
 
   in (rows, cols, squares)
