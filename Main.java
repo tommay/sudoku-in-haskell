1c53951af51626ea485bdfecfa339bf975d6c6f2
Tom May <tom@tommay.net>
2016-12-12 12:30:30 -0800

    Add Creater.createWithSolution.

--- Main.java
+++ Main.java
@@ -2,9 +2,15 @@ package net.tommay.spudoku;
 
 import net.tommay.spudoku.CreaterForJava;
 
+import frege.prelude.PreludeBase.TTuple2;
+import frege.runtime.Delayed;
+
 public class Main {
     public static void main (String[] args) {
-        String puzzle = CreaterForJava.create(1, args[0]);
-        System.out.println(puzzle);
+        TTuple2 t = CreaterForJava.create(1, args[0]);
+        String puzzle = Delayed.<String>forced(t.mem1);
+        String solution = Delayed.<String>forced(t.mem2);
+        System.out.println("Puzzle:   " + puzzle);
+        System.out.println("Solution: " + solution);
     }
 }
