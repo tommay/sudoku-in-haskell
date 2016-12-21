cb24355800339393b55bcab21493b3973a4ec648
Tom May <tom@tommay.net>
2016-12-20 19:18:44 -0800

    Add getExcludedCellSet and getExcludedCellList.

--- ExclusionSet.hs
+++ ExclusionSet.hs
@@ -8,10 +8,14 @@ module ExclusionSet (
   squares,
   name,
   cells,
+  getExcludedCellSet,
+  getExcludedCellList,
 ) where
 
 import           Data.Set (Set)
 import qualified Data.Set as Set
+import           Data.Vector (Vector)
+import qualified Data.Vector as Vector
 
 data ExclusionSet = ExclusionSet {
   name :: String,
@@ -60,3 +64,29 @@ square n =
       col = n `mod` 3 * 3
       cellNumbers = [(row + n `div` 3)*9 + (col + n `mod` 3) | n <- [0..8]]
   in make ("square " ++ show n) cellNumbers
+
+makeExcludedCellSet :: Int -> Set Int
+makeExcludedCellSet cellNumber =
+  let row = cellNumber `div` 9
+      col = cellNumber `mod` 9
+      rowList = ExclusionSet.cells $ ExclusionSet.row row
+      columnList = ExclusionSet.cells $ ExclusionSet.column col
+      squareList = ExclusionSet.cells $
+        ExclusionSet.square (((row `div` 3) * 3) + (col `div` 3))
+  in Set.delete cellNumber $ Set.fromList (rowList ++ columnList ++ squareList)
+
+excludedCellSets :: Vector (Set Int)
+excludedCellSets =
+  Vector.fromList [makeExcludedCellSet n | n <- [0..80]]
+
+getExcludedCellSet :: Int -> Set Int
+getExcludedCellSet cellNumber =
+  excludedCellSets Vector.! cellNumber
+
+excludedCellLists :: Vector [Int]
+excludedCellLists =
+  fmap Set.toList excludedCellSets
+
+getExcludedCellList :: Int -> [Int]
+getExcludedCellList cellNumber =
+  excludedCellLists Vector.! cellNumber
